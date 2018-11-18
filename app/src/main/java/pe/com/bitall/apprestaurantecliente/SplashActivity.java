5fbf961a9a54c2b1138aa99e85f77b5af9aebecc
lsotor@pucp.pe <lsotor@pucp.pe>
2018-11-18 12:06:46 -0500

    commit inicial

---
+++ app/src/main/java/pe/com/bitall/apprestaurantecliente/SplashActivity.java
@@ -0,0 +1,13 @@
+package pe.com.bitall.apprestaurantecliente;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
